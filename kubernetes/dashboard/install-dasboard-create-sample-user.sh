542fe79dc0a9a0274288d27349f3f93d5ab252b7
Vamsi <88853744+vkothapallirli@users.noreply.github.com>
2021-09-28 16:57:57 -0700

    Update install-dasboard-create-sample-user.sh

--- kubernetes/dashboard/install-dasboard-create-sample-user.sh
+++ kubernetes/dashboard/install-dasboard-create-sample-user.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.3.1/aio/deploy/recommended.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.3.1/aio/deploy/recommended.yaml && \
+ kubectl apply -f https://raw.githubusercontent.com/vamzi/HelperScripts/main/kubernetes/dashboard/admin-user.yaml && \
+ kubectl apply -f https://raw.githubusercontent.com/vamzi/HelperScripts/main/kubernetes/dashboard/admin-user-role.yaml && \
+ kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}"
